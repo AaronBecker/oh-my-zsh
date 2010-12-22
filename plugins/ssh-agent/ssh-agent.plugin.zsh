1b7c805fc67244bdc412347c4ae069bbe514ff2f
Aaron Becker <akbecker@gmail.com>
2010-12-22 17:49:26 -0600

    changed file sourcing to silence errors on startup

--- plugins/ssh-agent/ssh-agent.plugin.zsh
+++ plugins/ssh-agent/ssh-agent.plugin.zsh
@@ -6,14 +6,14 @@ local SSH_ENV=$HOME/.ssh/environment
 function start_agent {
   /usr/bin/env ssh-agent | sed 's/^echo/#echo/' > ${SSH_ENV}
   chmod 600 ${SSH_ENV}
-  . ${SSH_ENV} > /dev/null
+  source ${SSH_ENV} > /dev/null
   /usr/bin/ssh-add;
 }
 
 # Source SSH settings, if applicable
 
 if [ -f "${SSH_ENV}" ]; then
-  . ${SSH_ENV} > /dev/null
+  source ${SSH_ENV} > /dev/null
   ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
     start_agent;
   }
